94c54b52b0f585a5ae60fc676d183e8b145f6bce
aeterne <aeterne@gmail.com>
2013-06-22 19:44:05 +0200

    Testing application, learning RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazhar::Application.config.secret_token = '0ffe643987f00c11651d60ccede1298af3db8f9401ac7464a97a152519b4b0f49190fa160c1799849edd3272af1a3549d4fa14f89077e6cde8ad4736824d7ecf'
